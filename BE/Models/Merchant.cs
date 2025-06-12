0dbfb0baaa85e303414c996ef2c3ffcfd3c3b345
Ivana Borisova <ivana_borisova@abv.bg>
2025-06-12 13:56:47 +0300

    created models, usercontroller, db connection

---
+++ BE/Models/Merchant.cs
@@ -0,0 +1,14 @@
+﻿namespace SummerPracticeWebApi.Models
+{
+    public class Merchant
+    {
+        public int MerchantId { get; set; }
+        public string MerchantName
+        {
+            get; set;
+        }
+
+        public string MerchantDescription { get; set; }
+    }
+
+}
---
+++ BE/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SummerPracticeWebApi.Models
+{
+    [Table("users")]
+    public class User
+    {
+        [Column("id")]
+        
+        public int UserId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
