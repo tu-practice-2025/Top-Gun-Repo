5ad4d53de81749623d222554557710cce52604f5
Vasil Popov <32331759+vasil-popov@users.noreply.github.com>
2025-06-12 23:13:16 +0300

    Revert "Ivana/crud operations"

--- BE/Models/Budget.cs
+++ BE/Models/Budget.cs
@@ -1,17 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace SummerPracticeWebApi.Models
+﻿namespace SummerPracticeWebApi.Models
 {
     public class Budget
     {
-
-        [Column("id")]
         public int BudgetId { get; set; }
         
-        public int category_id { get; set; }
-        public int user_id { get; set; }
-        public DateTime date { get; set; }
-        public double limit { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+        public double Limit { get; set; }
       
     }
 }
