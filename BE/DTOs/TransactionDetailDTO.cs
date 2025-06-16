cf3b8c702c225badc955cdfc47ae4538f5c440bd
Ivana Borisova <ivana_borisova@abv.bg>
2025-06-16 15:44:22 +0300

    finished transaction details api

--- BE/DTOs/TransactionDetailDTO.cs
+++ BE/DTOs/TransactionDetailDTO.cs
@@ -6,7 +6,6 @@
         public string MerchantName { get; set; }    
         public string Iban { get; set; }            
         public double Amount { get; set; }
-
         public string CardNumber { get; set; }
     }
 }
