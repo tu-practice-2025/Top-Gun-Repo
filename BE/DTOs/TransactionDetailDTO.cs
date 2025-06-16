4e954ef8186faad81bbb0e17734f3b1d4ff3fb71
Ivana Borisova <ivana_borisova@abv.bg>
2025-06-16 13:14:03 +0300

    added api for detailspage

---
+++ BE/DTOs/TransactionDetailDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SummerPracticeWebApi.DTOs
+{
+    public class TransactionDetailDTO
+    {
+        public DateTime Date { get; set; }          
+        public string MerchantName { get; set; }    
+        public string Iban { get; set; }            
+        public double Amount { get; set; }
+
+        public string CardNumber { get; set; }
+    }
+}
