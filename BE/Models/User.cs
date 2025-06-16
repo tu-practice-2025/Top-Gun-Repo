a81dee48be04ee607d559ae488c3e18c751cd31b
fai7hless <vasilpopovxd@gmail.com>
2025-06-16 13:46:49 +0300

    Edited DB seed class, User and Transaction models and created a auth/login endpoint

--- BE/Models/User.cs
+++ BE/Models/User.cs
@@ -11,7 +11,7 @@ namespace SummerPracticeWebApi.Models
         public int UserId { get; set; }
 
         public string name { get; set; }
-        public string username { get; set; }
+        public string email { get; set; }
         public string password { get; set; }
 
 
